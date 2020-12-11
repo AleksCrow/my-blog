94dc38673f53aa9b816efc9d3e595d7ee529547e
AleksCrow <Qwerton00@gmail.com>
2020-12-11 12:33:10 +0200

    Prepare to deploy

--- src/main/java/com/voronkov/blog/config/MailSendConfig.java
+++ src/main/java/com/voronkov/blog/config/MailSendConfig.java
@@ -12,27 +12,45 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
-@PropertySource("classpath:/mail.properties")
+//@PropertySource("classpath:/mail.properties")
 public class MailSendConfig {
 
-  @Autowired
-  private Environment environment;
-
-  @Bean
-  public JavaMailSender getJavaMailSender() {
-    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-    mailSender.setHost("smtp.gmail.com");
-    mailSender.setPort(587);
-
-    mailSender.setUsername(environment.getProperty("spring.mail.username"));
-    mailSender.setPassword(environment.getProperty("spring.mail.password"));
-
-    Properties props = mailSender.getJavaMailProperties();
-    props.put("mail.transport.protocol", "smtp");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.starttls.enable", "true");
-    props.put("mail.debug", "true");
-
-    return mailSender;
-  }
+//  @Autowired
+//  private Environment environment;
+//
+//  @Bean
+//  public JavaMailSender getJavaMailSender() {
+//    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//    mailSender.setHost("smtp.gmail.com");
+//    mailSender.setPort(587);
+//
+//    mailSender.setUsername(environment.getProperty("spring.mail.username"));
+//    mailSender.setPassword(environment.getProperty("spring.mail.password"));
+//
+//    Properties props = mailSender.getJavaMailProperties();
+//    props.put("mail.transport.protocol", "smtp");
+//    props.put("mail.smtp.auth", "true");
+//    props.put("mail.smtp.starttls.enable", "true");
+//    props.put("mail.debug", "true");
+//
+//    return mailSender;
+//  }
+	
+	@Bean
+	  public JavaMailSender getJavaMailSender() {
+	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
+	
+	    mailSender.setUsername(System.getenv("EMAIL"));
+	    mailSender.setPassword(System.getenv("EMAIL_PASSWORD"));
+	
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
+	
+	    return mailSender;
+	  }
 }
