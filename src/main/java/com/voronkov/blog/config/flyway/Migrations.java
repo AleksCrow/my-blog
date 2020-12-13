6f7e8c0de3a152665ffee9ce1aba48fcf09fbda2
AleksCrow <Qwerton00@gmail.com>
2020-12-13 09:06:08 +0200

    Prepare to deploy

--- src/main/java/com/voronkov/blog/config/flyway/Migrations.java
+++ src/main/java/com/voronkov/blog/config/flyway/Migrations.java
@@ -7,9 +7,9 @@ public class Migrations {
 	public static void main(String[] args) throws Exception {
         Flyway flyway = Flyway
         		.configure()
-        		.dataSource(System.getenv("JDBC_DATABASE_URL"),
-        				System.getenv("JDBC_DATABASE_USERNAME"),
-        				System.getenv("JDBC_DATABASE_PASSWORD"))
+        		.dataSource(System.getenv("DATABASE_URL"),
+        				System.getenv("DATABASE_USERNAME"),
+        				System.getenv("DATABASE_PASSWORD"))
     			.load();
         flyway.migrate();
     }
