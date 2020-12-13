86f88142956ee90f46647e419ff3f84d8ae4af78
AleksCrow <Qwerton00@gmail.com>
2020-12-13 09:12:15 +0200

    Prepare to deploy

--- src/main/java/com/voronkov/blog/config/flyway/Migrations.java
+++ src/main/java/com/voronkov/blog/config/flyway/Migrations.java
@@ -1,15 +1,23 @@
 package com.voronkov.blog.config.flyway;
 
+import java.net.URI;
+
 import org.flywaydb.core.Flyway;
 
 public class Migrations {
 	
 	public static void main(String[] args) throws Exception {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+		
         Flyway flyway = Flyway
         		.configure()
-        		.dataSource(System.getenv("DATABASE_URL"),
-        				System.getenv("DATABASE_USERNAME"),
-        				System.getenv("DATABASE_PASSWORD"))
+        		.dataSource(System.getenv(dbUrl),
+        				System.getenv(username),
+        				System.getenv(password))
     			.load();
         flyway.migrate();
     }
