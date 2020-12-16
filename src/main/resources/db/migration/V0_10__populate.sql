2beef03ec044a8e29efb3fe998c84504c253af00
AleksCrow <Qwerton00@gmail.com>
2020-12-16 23:14:45 +0200

    Prepare to deploy

--- src/main/resources/db/migration/V0_10__populate.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (username, password, active, email, activation_code) VALUES
-	('admin', '$2y$07$GO2KkIBf8RHYUoMDOOEe7uYu9AGso1tiIfW9HL1/AZG2bauWZ0osW ', 'true', 'Qwerton00@gmail.com', 'null');
-	
-INSERT INTO user_roles (user_id, role) VALUES 
-	('1', 'USER'),
-	('1', 'ADMIN');
