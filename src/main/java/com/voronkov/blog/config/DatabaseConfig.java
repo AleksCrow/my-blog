a05227380de0cd155e37a9547714eeb577957c5a
AleksCrow <Qwerton00@gmail.com>
2020-12-16 11:05:11 +0200

    Prepare to deploy

--- src/main/java/com/voronkov/blog/config/DatabaseConfig.java
+++ src/main/java/com/voronkov/blog/config/DatabaseConfig.java
@@ -2,8 +2,18 @@ package com.voronkov.blog.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -12,11 +22,53 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @EnableTransactionManagement
 //@PropertySource("classpath:/postgres.properties")
 public class DatabaseConfig {
+	
+	@Value("${spring.datasource.url}")
+	  private String dbUrl;
+
+	  @Autowired
+	  private DataSource dataSource;
+
+	  @RequestMapping("/db")
+	  String db(Map<String, Object> model) {
+	    try (Connection connection = dataSource.getConnection()) {
+	      Statement stmt = connection.createStatement();
+	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+	      ArrayList<String> output = new ArrayList<String>();
+	      while (rs.next()) {
+	        output.add("Read from DB: " + rs.getTimestamp("tick"));
+	      }
+
+	      model.put("records", output);
+	      return "db";
+	    } catch (Exception e) {
+	      model.put("message", e.getMessage());
+	      return "error";
+	    }
+	  }
+
+	  @Bean
+	  public DataSource dataSource() throws SQLException {
+	    if (dbUrl == null || dbUrl.isEmpty()) {
+	      return new HikariDataSource();
+	    } else {
+	      HikariConfig config = new HikariConfig();
+	      config.setJdbcUrl(dbUrl);
+	      return new HikariDataSource(config);
+	    }
+	  }
 
 //  @Autowired
 //  private Environment environment;
@@ -34,21 +86,21 @@ public class DatabaseConfig {
 //    return driverManagerDataSource;
 //  }
 //  
-	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
+//	@Bean
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+//
+//        String username = dbUri.getUserInfo().split(":")[0];
+//        String password = dbUri.getUserInfo().split(":")[1];
+//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
 
   @Bean
   public JdbcTemplate applicationDataConnection() throws URISyntaxException {
