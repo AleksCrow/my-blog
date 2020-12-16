306435eb00418619bbef822578deb857538421ba
AleksCrow <Qwerton00@gmail.com>
2020-12-16 23:00:09 +0200

    Prepare to deploy

--- src/main/resources/db/migration/V0_10__populate.sql
+++ src/main/resources/db/migration/V0_10__populate.sql
@@ -1,6 +1,3 @@
-DELETE from users where username = 'admin';
-DELETE from users_roles where user_id = '1';
-
 INSERT INTO users (username, password, active, email, activation_code) VALUES
 	('admin', '$2y$07$GO2KkIBf8RHYUoMDOOEe7uYu9AGso1tiIfW9HL1/AZG2bauWZ0osW ', 'true', 'Qwerton00@gmail.com', 'null');
 	
