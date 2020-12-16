c92e758a827479a65dc30c9187aa4c54d7658a0e
AleksCrow <Qwerton00@gmail.com>
2020-12-16 11:14:30 +0200

    Prepare to deploy

--- src/main/java/com/voronkov/blog/config/DatabaseConfig.java
+++ src/main/java/com/voronkov/blog/config/DatabaseConfig.java
@@ -2,18 +2,8 @@ package com.voronkov.blog.config;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -22,53 +12,11 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @EnableTransactionManagement
 //@PropertySource("classpath:/postgres.properties")
 public class DatabaseConfig {
-	
-	@Value("${spring.datasource.url}")
-	  private String dbUrl;
-
-	  @Autowired
-	  private DataSource dataSource;
-
-	  @RequestMapping("/db")
-	  String db(Map<String, Object> model) {
-	    try (Connection connection = dataSource.getConnection()) {
-	      Statement stmt = connection.createStatement();
-	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-	      ArrayList<String> output = new ArrayList<String>();
-	      while (rs.next()) {
-	        output.add("Read from DB: " + rs.getTimestamp("tick"));
-	      }
-
-	      model.put("records", output);
-	      return "db";
-	    } catch (Exception e) {
-	      model.put("message", e.getMessage());
-	      return "error";
-	    }
-	  }
-
-	  @Bean
-	  public DataSource dataSource() throws SQLException {
-	    if (dbUrl == null || dbUrl.isEmpty()) {
-	      return new HikariDataSource();
-	    } else {
-	      HikariConfig config = new HikariConfig();
-	      config.setJdbcUrl(dbUrl);
-	      return new HikariDataSource(config);
-	    }
-	  }
 
 //  @Autowired
 //  private Environment environment;
@@ -101,19 +49,33 @@ public class DatabaseConfig {
 //
 //        return basicDataSource;
 //    }
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
   @Bean
   public JdbcTemplate applicationDataConnection() throws URISyntaxException {
-    return new JdbcTemplate(dataSource);
+    return new JdbcTemplate(dataSource());
   }
 
   @Bean
   public PlatformTransactionManager txManager() throws URISyntaxException {
-    return new DataSourceTransactionManager(dataSource);
+    return new DataSourceTransactionManager(dataSource());
   }
 
   @Bean
   public NamedParameterJdbcTemplate namedParameterJdbcTemplate() throws URISyntaxException {
-    return new NamedParameterJdbcTemplate(dataSource);
+    return new NamedParameterJdbcTemplate(dataSource());
   }
 }
